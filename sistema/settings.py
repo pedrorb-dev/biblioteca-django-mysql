10ac7a3670d9dcdb561555b422f00ba8e9854ccc
PedroDev <pedrulex.ramirez@gmail.com>
2025-11-15 21:09:53 -0600

    aqui hay un monton de errores

--- sistema/settings.py
+++ sistema/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca'
+    'biblioteca',
+    "admin_extra_buttons"
 ]
 
 MIDDLEWARE = [
